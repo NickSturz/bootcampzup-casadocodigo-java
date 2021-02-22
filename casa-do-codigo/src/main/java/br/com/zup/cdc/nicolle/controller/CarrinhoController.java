b9901847f736921e1eac142518398b170212880c
Nicolle Sturzbecher <62395184+NickSturz@users.noreply.github.com>
2021-02-22 10:59:01 -0300

    Projeto Casa do Código Simples

---
+++ casa-do-codigo/src/main/java/br/com/zup/cdc/nicolle/controller/CarrinhoController.java
@@ -0,0 +1,25 @@
+package br.com.zup.cdc.nicolle.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/casadocodigo/carrinho")
+public class CarrinhoController {
+	
+	
+//	@PostMapping
+//	public ResponseEntity<Carrinho> criaCarrinho(@RequestBody CarrinhoRequest request){
+//		Carrinho carrinho = request.novoCarrinho();
+//		
+//		System.out.println(carrinho);
+//		
+//		return ResponseEntity.ok().build();
+//		
+//		
+//	}
+	
+	
+	
+
+}
